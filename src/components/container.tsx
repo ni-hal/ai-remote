e78928263a8100d4bb5e5fa886bc17110b8da0e0
mishnan07 <mishnanc@gmail.com>
2025-09-16 13:27:10 +0530

    r

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -9,6 +9,8 @@ export const Container = ({ children, className }: ContainerProps) => {
   return (
     <div
       className={cn("container mx-auto px-4 md:px-8 py-4 w-full", className)}
+
+      
     >
       {children}
     </div>
